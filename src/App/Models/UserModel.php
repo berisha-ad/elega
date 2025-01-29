f304128e97611b036e6bb6d6103dd32f7cb4590b
Adrian Berisha <adrianberisha0@gmail.com>
2025-01-29 16:00:05 +0100

    modified Usermodel

--- src/App/Models/UserModel.php
+++ src/App/Models/UserModel.php
@@ -7,41 +7,46 @@ class UserModel extends Model {
     public static function userExists( string $username , string $email ) {
         $model = new self();
     
-        $model->db->query('SELECT * FROM users WHERE username = :username OR email = :email', [
+        $user = $model->db->query('SELECT * FROM users WHERE username = :username OR email = :email', [
             'username' => $username,
             'email' => $email
         ])->fetch();
 
-        return $model->db->rowCount() === 1;
+        return $user;
     }
 
     public static function create( string $username, string $email, string $password, string $city ) : bool {
         $model = new self();
 
-        $model->db->query('INSERT INTO users (username, email, password, city) VALUES (:username, :email, :password, :city)', [
+        $user = $model->db->query('INSERT INTO users (username, email, password, city) VALUES (:username, :email, :password, :city)', [
             'username' => $username,
             'email' => $email,
             'password' => hash('sha256', $password),
             'city' => $city
         ]);
 
-        return $model->db->rowCount() === 1;
+        if ($user) {
+            return true;
+        }
+        return false;
     }
 
+    
     public static function getUser( string $username ) {
         $model = new self();
 
-        $user =  $model->db->query('SELECT * FROM users WHERE username = :username', [
+        $user = $model->db->query('SELECT * FROM users WHERE username = :username', [
             "username" => $username
         ])->fetch();
         
         return $user;
     }
 
-    public static function getAllUsers() {
+    public static function getAllUsers() : array {
         $model = new self();
 
         $users = $model->db->query("SELECT * FROM users")->fetchAll();
+
         return $users;
     }
 }
